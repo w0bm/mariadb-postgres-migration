446b5edcd65fa6a35a2ba60c30185483cfe089d8
jkhsjdhjs <leon.moeller@yahoo.de>
2018-04-12 11:43:48 +0000

    add messages copying + serialise insertions as some columns reference others

--- main.mjs
+++ main.mjs
@@ -27,22 +27,28 @@ const set_auto_increment_and_cluster = (promise, table) => promise.then(result =
         ).catch(reject);
 }));
 
-console.log(["users", "videos", "comments"].map(t => "inserting " + t + "...").join("\n"));
-Promise.all([
+const insert_msg = arr => console.log(arr.map(t => "inserting " + t + "...").join("\n"));
 
-    //copy users
-      set_auto_increment_and_cluster(mydb.query("SELECT * FROM users")
-        .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO users(id, username, password, created_at, updated_at, deleted_at, banned, banreason, filters) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [r.id, r.username, cfg.password_placeholder, r.created_at, r.updated_at, null, r.banend, r.banreason, JSON.parse(r.categories)])))), "users")
-
-    //copy videos
-    , set_auto_increment_and_cluster(mydb.query("SELECT * FROM videos")
-        .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash) VALUES($1, $2, $3, $4, $5, $6)", [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash])))), "videos")
-
-    //copy comments
-    , set_auto_increment_and_cluster(mydb.query("SELECT * FROM comments")
-        .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)", [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at])))), "comments")
-
-])
+//copy users
+insert_msg(["users"]);
+set_auto_increment_and_cluster(mydb.query("SELECT * FROM users")
+    .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO users(id, username, password, created_at, updated_at, deleted_at, banned, banreason, filters) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)", [r.id, r.username, cfg.password_placeholder, r.created_at, r.updated_at, null, r.banend, r.banreason, JSON.parse(r.categories)])))), "users")
+//copy videos and messages
+.then(() => {
+    insert_msg(["videos", "messages"]);
+    return Promise.all([
+        set_auto_increment_and_cluster(mydb.query("SELECT * FROM messages")
+            .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO messages(id, from_user, to_user, title, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [r.id, r.from, r.to, r.subject, r.content, r.created_at, r.updated_at, r.deleted_at])))), "messages")
+      , set_auto_increment_and_cluster(mydb.query("SELECT * FROM videos")
+            .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash) VALUES($1, $2, $3, $4, $5, $6)", [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash])))), "videos")
+    ]);
+})
+//copy comments
+.then(() => {
+    insert_msg(["comments"]);
+    return set_auto_increment_and_cluster(mydb.query("SELECT * FROM comments")
+   .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)", [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at])))), "comments");
+})
 .catch(pretty_print) // error handling
 .then(() => { // close connections
     mydb.end();
