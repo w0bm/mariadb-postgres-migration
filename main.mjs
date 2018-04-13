a2814661c1fd0b1df7b85809f9abd4fe96e96c60
jkhsjdhjs <leon.moeller@yahoo.de>
2018-04-13 15:14:18 +0000

    add tagging of videos

--- main.mjs
+++ main.mjs
@@ -40,12 +40,20 @@ set_auto_increment_and_cluster(mydb.query("SELECT * FROM users")
             .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO messages(id, from_user, to_user, title, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [r.id, r.from, r.to, r.subject, r.content, r.created_at, r.updated_at, r.deleted_at])))), "messages")
       , set_auto_increment_and_cluster(mydb.query("SELECT * FROM videos")
             .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash) VALUES($1, $2, $3, $4, $5, $6)", [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash])))), "videos")
-            //copy comments
-            .then(() => {
+            .then(() => Promise.all([
+                //copy comments
+                (() => {
                 insert_msg(["comments"]);
                 return set_auto_increment_and_cluster(mydb.query("SELECT * FROM comments")
                     .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)", [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at])))), "comments");
-            })
+                })(),
+                //tag videos
+                (() => {
+                    console.log("tagging videos...");
+                    return mydb.query("SELECT v.id, tags.name, tags.normalized FROM videos as v JOIN taggable_taggables as vt ON v.id = vt.taggable_id JOIN taggable_tags as tags ON tags.tag_id = vt.tag_id")
+                        .then(rows => Promise.all(rows.map(r => pgdb.none("INSERT INTO tags(normalized, tag) VALUES($1, $2) ON CONFLICT DO NOTHING", [r.normalized, r.name]).then(pgdb.none("UPDATE videos SET tags = array_append(tags, $1) WHERE videos.id = $2", [r.name, r.id])))).then(() => console.log("all videos tagged")));
+                })()
+            ]))
     ]);
 })
 .catch(pretty_print) // error handling
