e94cd3e39ae31da20546852de4baada609873137
jkhsjdhjs <leon.moeller@yahoo.de>
2018-04-13 19:10:05 +0000

    improve video tagging

--- main.mjs
+++ main.mjs
@@ -12,26 +12,24 @@ const pretty_print = obj => console.log(JSON.stringify(obj, null, 2));
 //prints "inserting <table>..." for multiple tables
 const insert_msg = arr => console.log(arr.map(t => "inserting " + t + "...").join("\n"));
 
-//select tags and videos
-const select_videos_tags = "SELECT v.id, tags.name, tags.normalized FROM videos as v JOIN taggable_taggables as vt ON v.id = vt.taggable_id JOIN taggable_tags as tags ON tags.tag_id = vt.tag_id";
-
-//add tag to video
-const update_video_tags = "UPDATE videos SET tags = array_append(tags, $1) WHERE videos.id = $2";
+//select videos with their tags
+const select_videos_tags = `SELECT v.*, GROUP_CONCAT(DISTINCT t.normalized) as tags
+                                 FROM videos v, taggable_taggables tt, taggable_tags t
+                                 WHERE v.id = tt.taggable_id AND tt.tag_id = t.tag_id
+                                 GROUP BY v.id`
 
 //insert queries for shortening insert functions
 const insert_users = "INSERT INTO users(id, username, password, created_at, updated_at, deleted_at, banned, banreason, filters) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)";
 const insert_msgs = "INSERT INTO messages(id, from_user, to_user, title, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)";
-const insert_videos = "INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash) VALUES($1, $2, $3, $4, $5, $6)";
+const insert_videos = "INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash, tags) VALUES($1, $2, $3, $4, $5, $6, $7)";
 const insert_comments = "INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)";
-const insert_tags = "INSERT INTO tags(normalized, tag) VALUES($1, $2) ON CONFLICT DO NOTHING";
+const insert_tags = "INSERT INTO tags(normalized, tag) VALUES($1, $2)";
 
 //maps for shortening query functions
 const map_user = r => [r.id, r.username, cfg.password_placeholder, r.created_at, r.updated_at, null, r.banend, r.banreason, JSON.parse(r.categories)];
 const map_msg = r => [r.id, r.from, r.to, r.subject, r.content, r.created_at, r.updated_at, r.deleted_at];
-const map_video = r => [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash];
+const map_video = r => [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash, r.tags.split(",").map(t => t.substring(0, 30))];
 const map_comment = r => [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at];
-const map_tags = r => [r.normalized.substring(0, 30), r.name.substring(0, 30)];
-const map_tags_video = r => [r.name.substring(0, 30), r.id];
 
 //helper function to avoid redundant code
 const set_auto_increment_and_cluster = (promise, table) => promise.then(result => {
@@ -51,7 +49,7 @@ set_auto_increment_and_cluster(mydb.query("SELECT * FROM users").then(rows => Pr
         insert_msg(["videos", "messages"]);
         return Promise.all([
             set_auto_increment_and_cluster(mydb.query("SELECT * FROM messages").then(rows => Promise.all(rows.map(r => pgdb.none(insert_msgs, map_msg(r))))), "messages"),
-            set_auto_increment_and_cluster(mydb.query("SELECT * FROM videos").then(rows => Promise.all(rows.map(r => pgdb.none(insert_videos, map_video(r))))), "videos")
+            set_auto_increment_and_cluster(mydb.query(select_videos_tags).then(rows => Promise.all(rows.map(r => pgdb.none(insert_videos, map_video(r))))), "videos")
                 .then(() => Promise.all([
                     //copy comments
                     (() => {
@@ -61,8 +59,8 @@ set_auto_increment_and_cluster(mydb.query("SELECT * FROM users").then(rows => Pr
                     //tag videos
                     (() => {
                         console.log("tagging videos...");
-                        return mydb.query(select_videos_tags)
-                            .then(rows => Promise.all(rows.map(r => pgdb.none(insert_tags, map_tags(r)).then(pgdb.none(update_video_tags, map_tags_video(r))))).then(() => console.log("all videos tagged")));
+                        return pgdb.any("SELECT DISTINCT UNNEST(tags) FROM videos")
+                            .then(rows => Promise.all(rows.map(r => pgdb.none(insert_tags, [r.unnest, r.unnest]).catch(pretty_print))).then(() => console.log("all videos tagged")));
                     })()
                 ]))
         ]);
