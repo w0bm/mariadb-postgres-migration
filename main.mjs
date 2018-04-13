591089d858a0698e7902aa905b4fd7944cc02976
SW <Flummi@users.noreply.github.com>
2018-04-13 21:50:42 +0200

    :^)

--- main.mjs
+++ main.mjs
@@ -3,8 +3,8 @@ import pg from "pg-promise";
 import { default as cfg } from "./config.json";
 
 //create db connections
-const pgdb = (new pg())(cfg.postgres);
-const mydb = my.createPool(cfg.mysql);
+const pgdb = (new pg())(cfg.postgres)
+    , mydb = my.createPool(cfg.mysql);
 
 //function for printing an objects (mainly errors) in a readable way
 const pretty_print = obj => console.log(JSON.stringify(obj, null, 2));
@@ -16,20 +16,20 @@ const insert_msg = arr => console.log(arr.map(t => "inserting " + t + "...").joi
 const select_videos_tags = `SELECT v.*, GROUP_CONCAT(DISTINCT t.normalized) as tags
                             FROM videos v, taggable_taggables tt, taggable_tags t
                             WHERE v.id = tt.taggable_id AND tt.tag_id = t.tag_id
-                            GROUP BY v.id`
+                            GROUP BY v.id`;
 
 //insert queries for shortening insert functions
-const insert_users = "INSERT INTO users(id, username, password, created_at, updated_at, deleted_at, banned, banreason, filters) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)";
-const insert_msgs = "INSERT INTO messages(id, from_user, to_user, title, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)";
-const insert_videos = "INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash, tags) VALUES($1, $2, $3, $4, $5, $6, $7)";
-const insert_comments = "INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)";
-const insert_tags = "INSERT INTO tags(normalized, tag) VALUES($1, $2)";
+const insert_users = "INSERT INTO users(id, username, password, created_at, updated_at, deleted_at, banned, banreason, filters) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+    , insert_msgs = "INSERT INTO messages(id, from_user, to_user, title, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8)"
+    , insert_videos = "INSERT INTO videos(id, file, created_at, updated_at, deleted_at, hash, tags) VALUES($1, $2, $3, $4, $5, $6, $7)"
+    , insert_comments = "INSERT INTO comments(id, user_id, video_id, content, created_at, updated_at, deleted_at) VALUES($1, $2, $3, $4, $5, $6, $7)"
+    , insert_tags = "INSERT INTO tags(normalized, tag) VALUES($1, $2)";
 
 //maps for shortening query functions
-const map_user = r => [r.id, r.username, cfg.password_placeholder, r.created_at, r.updated_at, null, r.banend, r.banreason, JSON.parse(r.categories)];
-const map_msg = r => [r.id, r.from, r.to, r.subject, r.content, r.created_at, r.updated_at, r.deleted_at];
-const map_video = r => [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash, r.tags.split(",").map(t => t.substring(0, 30))];
-const map_comment = r => [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at];
+const map_user = r => [r.id, r.username, cfg.password_placeholder, r.created_at, r.updated_at, null, r.banend, r.banreason, JSON.parse(r.categories)]
+    , map_msg = r => [r.id, r.from, r.to, r.subject, r.content, r.created_at, r.updated_at, r.deleted_at]
+    , map_video = r => [r.id, r.file, r.created_at, r.updated_at, r.deleted_at, r.hash, r.tags.split(",").map(t => t.substring(0, 30))]
+    , map_comment = r => [r.id, r.user_id, r.video_id, r.content, r.created_at, r.updated_at, r.deleted_at];
 
 //helper function to avoid redundant code
 const set_auto_increment_and_cluster = (promise, table) => promise.then(result => {
