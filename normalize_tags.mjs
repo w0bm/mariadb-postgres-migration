8145fcb433ea48e8b2d7aca48de2d769b87b40db
SW <Flummi@users.noreply.github.com>
2018-04-17 22:33:07 +0200

    Update normalize_tags.mjs

--- normalize_tags.mjs
+++ normalize_tags.mjs
@@ -1,10 +1,10 @@
 import cp from "child_process";
 
 export default (tags, bufferSize) => new Promise((resolve, reject) => {
-    let child = cp.exec("normalize/target/release/normalize", { maxBuffer: bufferSize }, (err, stdout, stderr) => {
+    const child = cp.exec("normalize/target/release/normalize", { maxBuffer: bufferSize }, (err, stdout, stderr) => {
         if(!err && !stderr.length) {
-            let normalized_tags = stdout.split("\n"),
-                map = new Map();
+            let normalized_tags = stdout.split("\n")
+              , map = new Map();
             normalized_tags.pop();
             tags.forEach((t, i) => map.set(t, normalized_tags[i]));
             resolve(map);
