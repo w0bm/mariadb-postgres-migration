95f5ac53ccb413586e7709ce27d7ee29a564df45
jkhsjdhjs <leon.moeller@yahoo.de>
2018-04-17 22:27:54 +0200

    rewrite code using async/await update normalize submodule

--- main.mjs
+++ main.mjs
@@ -15,121 +15,142 @@ const pgp = new pg()
 const log = {
     copy_start: type => console.log("copying " + type + "..."),
     copy_done: (type, count) => console.log((count ? count + " " : "") + type + " copied")
-}
+};
 
+const copy_users = async () => {
+    log.copy_start("users");
+    let users = await mydb.query(queries.my.users);
+    users.map(u => {
+        u.password = cfg.password_placeholder;
+        u.categories = JSON.parse(u.categories);
+    });
+    return pgdb.none(pgh.insert(users, column_sets.users))
+        .then(() => log.copy_done("users", users.length));
+};
 
-//copy users and start timer
-console.time("done! elapsed time");
-log.copy_start("users");
-mydb.query(queries.my.users).then(rows => {
-        rows.map(r => {
-            r.password = cfg.password_placeholder;
-            r.categories = JSON.parse(r.categories);
-        });
-        return pgdb.none(pgh.insert(rows, column_sets.users))
-            .then(() => log.copy_done("users", rows.length));
-    })
-    //normalize tags, then copy videos and tags
-    .then(() => {
-        log.copy_start("videos");
-        return mydb.query(queries.my.videos_with_tags, cfg.tag_select_separator)
-            .then(rows => normalize_tags(
-                    rows.map(r => r.tags.split(cfg.tag_select_separator))
-                        .reduce((cur, nxt) => cur.concat(nxt)),
-                    cfg.tag_normalize_buffer
-                )
-                .then(tag_map => {
-                    rows.map(r => r.tags = r.tags
-                        .split(cfg.tag_select_separator)
-                        .map(t => tag_map.get(t))
-                    );
-                    return pgdb.none(pgh.insert(rows, column_sets.videos))
-                        .then(() => {
-                            log.copy_done("videos", rows.length);
-                            log.copy_start("tags");
-                            return pgdb.none(pgh.insert(
-                                [...tag_map].map(pair => Object.assign(
-                                    {},
-                                    {
-                                        "tag": pair[0].substring(0, 30),
-                                        "normalized": pair[1].substring(0, 30)
-                                    }
-                                )),
-                                column_sets.tags
-                            ) + " ON CONFLICT DO NOTHING")
-                            .then(() => log.copy_done("tags"));
-                        });
+const copy_videos_and_tags = async () => {
+    log.copy_start("videos");
+    let videos = await mydb.query(queries.my.videos_with_tags, cfg.tag_select_separator);
+    console.log("normalizing tags...");
+    const tag_map = await normalize_tags(
+        videos.map(v =>v.tags.split(cfg.tag_select_separator))
+            .reduce((cur, nxt) => cur.concat(nxt)),
+        cfg.tag_normalize_buffer
+    );
+    console.log("normalized tags")
+    videos.map(v =>
+        v.tags = v.tags
+            .split(cfg.tag_select_separator)
+            .map(t => tag_map.get(t))
+    );
+    await pgdb.none(pgh.insert(videos, column_sets.videos));
+    log.copy_done("videos", videos.length);
+    log.copy_start("tags");
+    return pgdb.none(pgh.insert(
+        [...tag_map].map(pair => Object.assign(
+            {},
+            {
+                "tag": pair[0].substring(0, 30),
+                "normalized": pair[1].substring(0, 30)
+            }
+        )),
+        column_sets.tags
+    ) + " ON CONFLICT DO NOTHING")
+        .then(() => log.copy_done("tags"));
+};
+
+const copy_comments = async () => {
+    log.copy_start("comments");
+    const comments = await mydb.query(queries.my.comments);
+    return pgdb.none(pgh.insert(comments, column_sets.comments))
+        .then(() => log.copy_done("comments", comments.length));
+};
+
+const copy_messages = async () => {
+    log.copy_start("messages");
+    const messages = await mydb.query(queries.my.messages);
+    return pgdb.none(pgh.insert(messages, column_sets.messages))
+        .then(() => log.copy_done("messages", messages.length));
+};
+
+//links uploads and favorites to their respective users
+const fill_playlists = async () => {
+    log.copy_start("uploads into playlist_video");
+    log.copy_start("favorites into playlist_video");
+    const playlists = await pgdb.any(queries.pg.playlists);
+    const fill_playlists = async (select_query, playlist_title) => {
+        const user_playlist = new Map(playlists
+            .filter(pl => pl.title === playlist_title)
+            .map(pl => [pl.user_id, pl.id]
+                .map(str => parseInt(str))
+            )
+        );
+        const videos = await mydb.query(select_query);
+        return pgdb.none(pgh.insert(
+            videos.map(v =>
+                Object.assign({}, {
+                    playlist_id: user_playlist.get(v.user_id),
+                    video_id: v.id || v.video_id,
+                    created_at: v.created_at
                 })
-            );
-    })
-    //copy comments and messages
-    .then(() => {
-        log.copy_start("comments");
-        log.copy_start("messages");
-        return Promise.all([
-            mydb.query(queries.my.comments).then(rows =>
-                pgdb.none(pgh.insert(rows, column_sets.comments))
-                    .then(() => log.copy_done("comments", rows.length))
             ),
-            mydb.query(queries.my.messages).then(rows =>
-                pgdb.none(pgh.insert(rows, column_sets.messages))
-                    .then(() => log.copy_done("messages", rows.length))
-            )
+            column_sets.playlist_video
+        ))
+    };
+    return Promise.all([
+        fill_playlists(queries.my.favorites, "Favorites")
+            .then(() => log.copy_done("favorites")),
+        fill_playlists(queries.my.uploads, "Uploads")
+            .then(() => log.copy_done("uploads"))
+    ]);
+};
+
+const set_auto_increment = async tables => Promise.all(
+    tables.map(table => pgdb.any(queries.pg.max_id(table))
+        .then(row => pgdb.none(queries.pg.set_auto_increment(table, parseInt(row[0].id) + 1))
+            .then(() => console.log("adjusted auto increment for " + table + " table"))
+        )
+    )
+);
+
+const cluster_tables = async tables => Promise.all(
+    tables.map(table => pgdb.none(queries.pg.cluster_pkey(table))
+        .then(() => console.log("clustered " + table + " table"))
+    )
+);
+
+(async () => {
+    try {
+        console.time("done! elapsed time");
+        await copy_users();
+        await copy_videos_and_tags();
+        await Promise.all([
+            copy_comments(),
+            copy_messages(),
+            fill_playlists()
         ]);
-    })
-    //link user favorites and uploads
-    .then(() => {
-        log.copy_start("uploads into playlist_video");
-        log.copy_start("favorites into playlist_video");
-        return pgdb.any(queries.pg.playlists).then(playlists => {
-            const fill_playlists = (select_query, playlist_title) => {
-                const user_playlist = new Map(playlists
-                    .filter(pl => pl.title === playlist_title)
-                    .map(pl => [pl.user_id, pl.id]
-                        .map(str => parseInt(str))
-                    )
-                );
-                return mydb.query(select_query).then(videos => {
-                    pgdb.none(pgh.insert(
-                        videos.map(v =>
-                            Object.assign({}, {
-                                playlist_id: user_playlist.get(v.user_id),
-                                video_id: v.id || v.video_id,
-                                created_at: v.created_at
-                            })
-                        ),
-                        column_sets.playlist_video
-                    ))
-                });
-            };
-            return Promise.all([
-                fill_playlists(queries.my.favorites, "Favorites")
-                    .then(() => log.copy_done("favorites")),
-                fill_playlists(queries.my.uploads, "Uploads")
-                    .then(() => log.copy_done("uploads"))
-            ]);
-        });
-    })
-    //set auto increment because ids may be missing in between in origin table
-    .then(() => Promise.all(["comments", "messages", "users", "videos"]
-            .map(table => pgdb.any(queries.pg.max_id(table))
-                .then(row => pgdb.none(queries.pg.set_auto_increment(table, parseInt(row[0].id) + 1))
-                    .then(() => console.log("adjusted auto increment for " + table + " table"))
-                )
-            )
-    ))
-    //cluster all tables to make sure order is correct
-    .then(() => Promise.all(["comments", "messages", "playlist_video", "playlists", "users", "videos"]
-            .map(table => pgdb.none(queries.pg.cluster_pkey(table))
-                .then(() => console.log("clustered " + table + " table"))
-            )
-    ))
-    //catch errors
-    .catch(console.log)
-    //close database connections and print elapsed time
-    .then(() => {
+        await set_auto_increment([
+            "comments",
+            "messages",
+            "users",
+            "videos"
+        ]);
+        await cluster_tables([
+            "comments",
+            "messages",
+            "playlist_video",
+            "playlists",
+            "users",
+            "videos"
+        ]);
+        console.timeEnd("done! elapsed time");
+    }
+    catch(error) {
+        console.log(error);
+    }
+    finally {
         console.log("closing db connections...");
         mydb.end();
         pgdb.$pool.end();
-        console.timeEnd("done! elapsed time");
-    });
+    }
+})();
